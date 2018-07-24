0501c0aa999ea7729bb8fd15e5cb9d2fdf3c236c
hadi <m.hadi.hojjat@gmail.com>
2018-07-24 14:32:37 +0430

    mongo initialized

---
+++ utils/mongo.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  'mongodb://localhost:27017/utgraph',
+  { useNewUrlParser: true },
+  err => {
+    if (err) {
+      console.log(err);
+    } else console.log('connected');
+  },
+);
+
+const UserSchema = require('../models/User');
+
+mongoose.model('User', UserSchema);
+
+module.exports = mongoose;
