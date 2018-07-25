f562e3dd1631d132e27c0e10fe964ba2a872a490
Arman Rostami <arman.rostami.999@gmail.com>
2018-07-26 00:35:29 +0430

    simple login route set up without jwt

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,22 @@
 var express = require('express');
+var mongoose = require('./utils/mongo');
 var router = express.Router();
+var User = mongoose.model('User');
+
+router.post('/login', function(req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  User.findOne({ username: username, password: password }, function(err, user) {
+    if (err) {
+      console.log(err);
+      res.status(500).send();
+    }
+
+    if (!user) {
+      return res.status(404).send();
+    }
+  });
+});
 
 module.exports = router;
