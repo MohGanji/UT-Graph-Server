dca24dc34ac8b49dffefb9fc9ed0ec9574e1cc46
Arman Rostami <arman.rostami.999@gmail.com>
2018-07-26 01:09:26 +0430

    api route added to app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var api = require('./routes/api');
 
 const mongoose = require('./utils/mongo');
 const User = mongoose.model('User');
@@ -41,6 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
