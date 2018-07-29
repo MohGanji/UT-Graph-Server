ab39dfbd0a600cd3a4ca006486a69d80db3669ff
Arman Rostami <arman.rostami.999@gmail.com>
2018-07-29 14:24:51 +0430

    Add toJSON to UserSchema

--- models/User.js
+++ models/User.js
@@ -8,6 +8,14 @@ var UserSchema = mongoose.Schema({
   email: String,
 });
 
+UserSchema.methods.toJSON = function() {
+  var obj = this.toObject();
+  delete obj.password;
+  delete obj._id;
+  delete obj.__v;
+  return obj;
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = User;
