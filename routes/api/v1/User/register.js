fce0e9eda183c872d187d796c4fd8ced3fad4f63
Arman Rostami <arman.rostami.999@gmail.com>
2018-07-31 11:33:51 +0430

    Changer register route style to async

--- routes/api/v1/User/register.js
+++ routes/api/v1/User/register.js
@@ -3,32 +3,28 @@ var mongoose = require('../../../../utils/mongo');
 var User = require('../../../../models/User');
 var router = express.Router();
 
-router.post('/', async function(req, res) {
+router.post('/', async function (req, res) {
   let username = req.body.data.username;
   let password = req.body.data.password;
   let firstName = req.body.data.firstName;
   let lastName = req.body.data.lastName;
   let email = req.body.data.email;
 
-  await User.findOne({ username: username }, function(err, user) {
-    if (user) {
-      return res.status(400).send('Duplicate User'); //to be changed later
-    } else {
-      User.create(
-        {
-          username: username,
-          password: password,
-          firstName: firstName,
-          lastName: lastName,
-          email: email,
-        },
-        function(err) {
-          console.log(err);
-          return res.status(500).send();
-        },
-      );
-    }
-  });
+  let user = await User.findOne({ username: username });
+
+  if (user) {
+    return res.status(400).send();
+  } else {
+    await User.create(
+      {
+        username: username,
+        password: password,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+      },
+    ).catch((err) => res.status(500).send());
+  }
   return res.status(200).send();
 });
 
