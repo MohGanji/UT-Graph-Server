3f745147d64af84bfd7c08b339b3602b4a37f2a7
Arman Rostami <arman.rostami.999@gmail.com>
2018-08-01 14:10:02 +0430

    Fix register user syntax

--- routes/api/v1/User/register.js
+++ routes/api/v1/User/register.js
@@ -5,26 +5,20 @@ var router = express.Router();
 
 router.post('/', async function (req, res) {
   let username = req.body.data.username;
-  let password = req.body.data.password;
-  let firstName = req.body.data.firstName;
-  let lastName = req.body.data.lastName;
-  let email = req.body.data.email;
+  let newUser = req.body.data;
 
   let user = await User.findOne({ username: username });
-
   if (user) {
     return res.status(400).send();
-  } else {
-    await User.create(
-      {
-        username: username,
-        password: password,
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-      },
-    ).catch((err) => res.status(500).send());
   }
+
+  try {
+    await User.create(newUser)
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send();
+  }
+
   return res.status(200).send();
 });
 
