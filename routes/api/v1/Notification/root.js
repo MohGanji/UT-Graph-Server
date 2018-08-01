c33a008191c73be49e317c0b2843c1da2b85f0f9
Arman Rostami <arman.rostami.999@gmail.com>
2018-08-01 21:14:32 +0430

    Add get notification leaf

--- routes/api/v1/Notification/root.js
+++ routes/api/v1/Notification/root.js
@@ -7,14 +7,29 @@ var isAuthenticated = require('../../../../middlewares/verifyJWTToken').verifyJW
 router.get('/', isAuthenticated, async function (req, res) {
   var username = req.username;
   var user = await User.findOne({ username: username }).catch((err) => res.status(500).send());
-
   var userId = user._id;
+
   var notifications = await NotificationUser.find({ user: userId }).catch((err) => res.status(500).send());
   var mappedNotifications = await Promise.all(notifications.map(async function () {
     return await NotificationUser.toJSON();
   }));
 
   res.status(200).send(JSON.stringify({ data: mappedNotifications }));
+});
+
+router.get('/:id', isAuthenticated, async function (req, req) {
+  var username = req.username;
+  var user = await User.findOne({ username: username }).catch((err) => res.status(500).send());
+  var userId = user._id;
+  var notificationId = req.params.id;
+
+  var notification = await NotificationUser.findById(notificationId).catch((err) => res.status(500).send());
+  if (notification.user != userId) {
+    return res.status(401).send();
+  }
+  else {
+    return res.status(200).send(JSON.stringify({ data: notification }));
+  }
 });
 
 module.exports = router;
