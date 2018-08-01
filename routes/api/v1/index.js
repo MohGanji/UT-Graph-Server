0e2fc9c4d6af96582552ab47545e540bf1db4c19
Arman Rostami <arman.rostami.999@gmail.com>
2018-08-01 14:22:41 +0430

    Delete auth requirement!

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var user = require('./User');
 var event = require('./Event');
-var auth = require('./Auth');
 
 router.use('/user', user);
 router.use('/event', event);
