88e505cc3493ff010fc3630c590df48eb2715a4d
Arman Rostami <arman.rostami.999@gmail.com>
2018-08-02 11:36:10 +0430

    Add index to user

--- models/User.js
+++ models/User.js
@@ -8,7 +8,7 @@ var UserSchema = mongoose.Schema({
   email: String,
 });
 
-UserSchema.methods.toJSON = function() {
+UserSchema.methods.toJSON = function () {
   var obj = this.toObject();
   delete obj.password;
   delete obj._id;
@@ -16,6 +16,6 @@ UserSchema.methods.toJSON = function() {
   return obj;
 };
 
-var User = mongoose.model('User', UserSchema);
+UserSchema.index({ username: 'text', firstName: 'text', lastName: 'text' });
 
-module.exports = User;
+module.exports = mongoose.model('User', UserSchema);
