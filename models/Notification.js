ec30b3f4c80162d0e066342da6f3c785fb52dd67
hadi <m.hadi.hojjat@gmail.com>
2018-08-07 16:00:24 +0430

    change Notification Schema

--- models/Notification.js
+++ models/Notification.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
 var NotificationSchema = mongoose.Schema({
-  message: String,
+  //  message: String,
   user: mongoose.Schema.Types.ObjectId,
-
   date: { type: Date, default: new Date() },
   read: Boolean,
+  type: {
+    type: String,
+    enum: ['REQUEST', 'ACCEPT', 'INFORMATION'],
+  },
+  applicant: mongoose.Schema.Types.ObjectId,
+  event: mongoose.Schema.Types.ObjectId,
 });
 
 NotificationSchema.methods.toJSON = function() {
