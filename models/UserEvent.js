fba5373fd381c122aa22c4ab1ecb897d9255eb42
hadi <m.hadi.hojjat@gmail.com>
2018-08-08 11:48:45 +0430

    add role field to UserEvent schema

--- models/UserEvent.js
+++ models/UserEvent.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 
 var UserEventSchema = mongoose.Schema({
   user: mongoose.Schema.Types.ObjectId,
+  role: {
+    type: String,
+    enum: ['STAFF', 'ATTENDENT'],
+  },
   event: mongoose.Schema.Types.ObjectId,
-  date: Date
+  date: Date,
 });
 
 module.exports = mongoose.model('UserEvent', UserEventSchema);
