db42c4eb7650ea76c4d6c8231239127db1375ae1
hadi <m.hadi.hojjat@gmail.com>
2018-08-08 11:45:15 +0430

    separating signup for staff and attendent

--- routes/api/v1/Event/root.js
+++ routes/api/v1/Event/root.js
@@ -1,57 +1,54 @@
 var express = require('express');
 var Event = require('../../../../models/Event');
 let UserEvent = require('../../../../models/UserEvent');
+let Notification = require('../../../../models/Notification');
 var router = express.Router();
-var isAuthenticated = require('../../../../middlewares/verifyJWTToken').verifyJWTToken;
+var isAuthenticated = require('../../../../middlewares/verifyJWTToken')
+  .verifyJWTToken;
 
-router.get('/', async function (req, res) {
+router.get('/', async function(req, res) {
+  var events = await Event.find({}).catch(err => res.status(500).send());
 
-  var events = await Event.find({}).catch((err) => res.status(500).send())
-
-  var mappedEvents = await Promise.all(events.map(async function (event) {
-    return (await event.toJSON());
-  }));
+  var mappedEvents = await Promise.all(
+    events.map(async function(event) {
+      return await event.toJSON();
+    }),
+  );
 
   res.status(200).send(JSON.stringify({ data: mappedEvents }));
 });
 
-router.post('/', isAuthenticated, async function (req, res) {
-
+router.post('/', isAuthenticated, async function(req, res) {
   let title = req.body.data.title;
   let beginTime = new Date(req.body.data.beginTime); //ok?
   let endTime = new Date(req.body.data.endTime);
   let organizer = req.username;
   let description = req.body.data.description;
 
-  await Event.create(
-    {
-      title: title,
-      beginTime: beginTime,
-      endTime: endTime,
-      organizer: organizer,
-      description: description,
-    }
-  ).catch((err) => res.status(500).send());
+  await Event.create({
+    title: title,
+    beginTime: beginTime,
+    endTime: endTime,
+    organizer: organizer,
+    description: description,
+  }).catch(err => res.status(500).send());
 
   return res.status(200).send();
 });
 
-router.get('/:id', async function (req, res) {
+router.get('/:id', async function(req, res) {
   let id = req.params.id;
   let event;
   try {
     event = await Event.findOne({ _id: id });
-  }
-  catch (err) {
+  } catch (err) {
     return res.status(500).send();
-  }
-  finally {
+  } finally {
     return res.status(200).send(JSON.stringify({ data: event.toJSON() }));
   }
 });
 
-router.put('/:id', isAuthenticated, async function (req, res) {
-
+router.put('/:id', isAuthenticated, async function(req, res) {
   var id = req.params.id;
   var event = await Event.findById(id);
   var updatedEvent = req.body.data;
@@ -65,22 +62,46 @@ router.put('/:id', isAuthenticated, async function (req, res) {
   return res.status(200).send();
 });
 
-router.post('/:id/signup', isAuthenticated, async function (req, res) {
+router.post('/:id/signup_staff', isAuthenticated, async function(req, res) {
   let username = req.username;
   let user = await user.findOne({ username: username });
   let userId = user._id;
   let eventId = req.params.id;
   let event = await Event.findOne({ _id: id });
 
+  await Notification.create({
+    user: event.organizer,
+    read: false,
+    type: 'REQUEST',
+    applicant: username,
+    event: event,
+  });
+
   await UserEvent.create({
     user: userId,
     event: eventId,
-    date: new Date()
+    role: 'STAFF',
+    date: new Date(),
   });
   return res.status(200).send();
 });
 
-router.delete('/:id', isAuthenticated, async function (req, res) {
+router.post('/:id/signup_attendent', isAuthenticated, async function(req, res) {
+  let username = req.username;
+  let user = await user.findOne({ username: username });
+  let userId = user._id;
+  let eventId = req.params.id;
+
+  await UserEvent.create({
+    user: userId,
+    event: eventId,
+    role: 'ATTENDENT',
+    date: new Date(),
+  });
+  return res.status(200).send();
+});
+
+router.delete('/:id', isAuthenticated, async function(req, res) {
   let username = req.username;
   let id = req.params.id;
   let event = await findById(id);
@@ -89,7 +110,7 @@ router.delete('/:id', isAuthenticated, async function (req, res) {
     return res.status(401).send();
   } else {
     await Event.findByIdAndDelete(id);
-    await UserEvent.remove({ event: event._id })
+    await UserEvent.remove({ event: event._id });
   }
-})
+});
 module.exports = router;
