7f84e81a459dd3e4f18049b1ca71e255d78a6410
hadi <m.hadi.hojjat@gmail.com>
2018-08-08 11:47:04 +0430

    add accept api for staff request

--- routes/api/v1/Notification/root.js
+++ routes/api/v1/Notification/root.js
@@ -24,7 +24,55 @@ router.get('/', isAuthenticated, async function(req, res) {
   res.status(200).send(JSON.stringify({ data: mappedNotifications }));
 });
 
+//   GET /:id/accept
+
+router.get('/:id/accept', isAuthenticated, async function(req, req) {
+  var username = req.username;
+  var user = await User.findOne({ username: username }).catch(err =>
+    res.status(500).send(),
+  );
+  var userId = user._id;
+
+  var notificationId = req.params.id;
+  var notification = await Notification.findById(notificationId).catch(err =>
+    res.status(500).send(),
+  );
+
+  if (notification.event.organizer != userId) {
+    return res.status(401).send();
+  } else {
+    await Notification.create({
+      user: notification.applicant,
+      read: false,
+      type: 'ACCEPT',
+      event: notification.event,
+    });
+
+    return res.status(200).send(JSON.stringify({ data: notification }));
+  }
+});
+
 router.get('/:id', isAuthenticated, async function(req, req) {
+  var username = req.username;
+  var user = await User.findOne({ username: username }).catch(err =>
+    res.status(500).send(),
+  );
+  var userId = user._id;
+
+  var notificationId = req.params.id;
+  var notification = await Notification.findById(notificationId).catch(err =>
+    res.status(500).send(),
+  );
+
+  if (notification.user != userId) {
+    return res.status(401).send();
+  } else {
+    return res.status(200).send(JSON.stringify({ data: notification }));
+  }
+});
+
+router.delete('/:id', isAuthenticated, async function(req, res) {
+  //TODO: find a better way to do this!
   var username = req.username;
   var user = await User.findOne({ username: username }).catch(err =>
     res.status(500).send(),
@@ -38,24 +86,9 @@ router.get('/:id', isAuthenticated, async function(req, req) {
   if (notification.user != userId) {
     return res.status(401).send();
   } else {
-    return res.status(200).send(JSON.stringify({ data: notification }));
-  }
-});
-
-/* router.delete('/:id', isAuthenticated, async function (req, res) { //TODO: find a better way to do this!
-  var username = req.username;
-  var user = await User.findOne({ username: username }).catch((err) => res.status(500).send());
-  var userId = user._id;
-  var notificationId = req.params.id;
-
-  var notification = await Notification.findById(notificationId).catch((err) => res.status(500).send());
-  if (notification.user != userId) {
-    return res.status(401).send();
-  }
-  else {
     await notification.remove(); //does it work correctly?
     return res.status(200).send(JSON.stringify({ data: notification }));
   }
-})  */
+});
 
 module.exports = router;
