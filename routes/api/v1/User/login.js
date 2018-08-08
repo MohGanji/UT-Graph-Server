ce73f68fa7cfe93ded1aeeb9e748ee244e703484
hadi <m.hadi.hojjat@gmail.com>
2018-08-08 12:20:30 +0430

    require User model in all files

--- routes/api/v1/User/login.js
+++ routes/api/v1/User/login.js
@@ -1,33 +1,36 @@
 var express = require('express');
 var mongoose = require('../../../../utils/mongo');
+var User = require('../../../../models/User');
 var config = require('../../../../utils/config');
 var jwt = require('jsonwebtoken');
 var router = express.Router();
-var User = mongoose.model('User');
 
 var secret = config.secret;
 
-router.post('/', async function (req, res) {
+router.post('/', async function(req, res) {
   var username = req.body.data.username;
   var password = req.body.data.password;
 
-  var user = await User.findOne({ username: username, password: password }).catch((err) => res.status(500).send());
+  var user = await User.findOne({
+    username: username,
+    password: password,
+  }).catch(err => res.status(500).send());
 
   if (!user) {
     return res.status(404).send();
   } else {
     var authenticationObj = {
       data: {
-        token: ''
-      }
-    }
+        token: '',
+      },
+    };
     authenticationObj.data.token = jwt.sign(
       {
         username: username,
       },
       secret,
-      { expiresIn: 60 * 60 }
-    )
+      { expiresIn: 60 * 60 },
+    );
     return res.json(authenticationObj);
   }
 });
