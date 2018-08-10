6ecbffb680db0ab09c74d8fabd46c4d21155efd3
Arman Rostami <arman.rostami.999@gmail.com>
2018-08-10 18:18:01 +0430

    Delete redundant user in event index

--- routes/api/v1/Event/index.js
+++ routes/api/v1/Event/index.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 var root = require('./root');
 var search = require('./search');
-var user = require('./user');
 
-router.use('/user', user);
 router.use('/search', search);
 router.use('/', root);
 
