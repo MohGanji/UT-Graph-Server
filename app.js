0758be701e909503e9d7f2df7c1f1c25620a08c2
hadi <m.hadi.hojjat@gmail.com>
2018-08-13 16:19:56 +0430

    set view engine to ejs

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const User = require('./models/User');
 var app = express();
 
 // sample create models
-app.get('/api/register', async function (req, res) {
+app.get('/api/register', async function(req, res) {
   console.log(req.query);
   let name = req.query.name;
   let year = req.query.year;
@@ -24,7 +24,7 @@ app.get('/api/register', async function (req, res) {
       name: name,
       password: '123',
     },
-    function (err) {
+    function(err) {
       console.log(err);
     },
   );
@@ -33,7 +33,7 @@ app.get('/api/register', async function (req, res) {
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -46,14 +46,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', api);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.set('view engine', 'jade');
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
