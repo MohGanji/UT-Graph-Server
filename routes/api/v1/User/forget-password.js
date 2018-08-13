5e7fa635582d487743306238c0002d001af1ba7e
hadi <m.hadi.hojjat@gmail.com>
2018-08-13 16:22:03 +0430

    design api forget-password

---
+++ routes/api/v1/User/forget-password.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var User = require('../../../../models/User');
+var constLink = require('../../../../utils/config').constLink;
+
+var router = express.Router();
+
+router.get('/', async function(req, res) {
+  res.render('forget_page.ejs');
+});
+
+router.post('/sent_email', async function(req, res) {
+  let email = req.body.email;
+  let user = await User.findOne({ email: email });
+
+  let data = user.username;
+  let buff = new Buffer(data);
+  let base64data = buff.toString('base64');
+  const link = constLink + '?user=' + base64data;
+  console.log(link);
+});
+
+module.exports = router;
