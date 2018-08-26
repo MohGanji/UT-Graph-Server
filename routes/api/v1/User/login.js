f8b2e393e5e2070d495b9c5c25d21df09e9bca1c
hadi <m.hadi.hojjat@gmail.com>
2018-08-26 17:50:42 +0430

    vlidation login

--- routes/api/v1/User/login.js
+++ routes/api/v1/User/login.js
@@ -5,16 +5,40 @@ var config = require('../../../../utils/config');
 var jwt = require('jsonwebtoken');
 var router = express.Router();
 
+const { check, validationResult } = require('express-validator/check');
+
 var secret = config.secret;
 
-router.post('/', async function(req, res) {
+router.post('/', [
+  check('data.username').custom(async (value, { req }) => {
+    let user = await User.findOne({ username: value });
+    if (!user) {
+      throw new Error('username not found!');
+    }
+    if (user.password != req.body.data.password) {
+      throw new Error('password is incorrect!');
+    }
+  }),
+], async function (req, res) {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log("error");
+    return res.status(422).json({ errors: errors.array() });
+  }
+  console.log(validationResult(req).array());
+
   var username = req.body.data.username;
   var password = req.body.data.password;
 
-  var user = await User.findOne({
-    username: username,
-    password: password,
-  }).catch(err => res.status(500).send());
+  try {
+    var user = await User.findOne({
+      username: username,
+      password: password,
+    })
+  } catch (err) {
+    res.status(500).send();
+  }
 
   if (!user) {
     return res.status(404).send();
