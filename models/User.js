be47a2729c5997752da7616932580f161b74727d
hadi <m.hadi.hojjat@gmail.com>
2018-08-31 00:32:44 +0430

    donot delete _id from User schema

--- models/User.js
+++ models/User.js
@@ -12,7 +12,6 @@ var UserSchema = mongoose.Schema({
 UserSchema.methods.toJSON = function () {
   var obj = this.toObject();
   delete obj.password;
-  delete obj._id;
   delete obj.__v;
   return obj;
 };
