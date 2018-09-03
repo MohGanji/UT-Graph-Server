a1a32c51892b015383917148668d43c669a54974
Mojtaba Shahbazi <mojtabashahbazi1377@gmail.com>
2018-09-04 03:27:02 +0430

    upload client sample added

--- routes/api/v1/User/index.js
+++ routes/api/v1/User/index.js
@@ -6,46 +6,17 @@ var register = require('./register');
 var reset_password = require('./reset-password');
 var forget_password = require('./forget-password');
 var search = require('./search');
-// var fs = require('fs');
-// var base64 = new Buffer(req[body], 'base64');
-// fs.writeFile(savePath + filename, base64, function (error) {
-// });
-const uuid = require('uuid')
-const path = require('path');
+var upload = require('./upload');
+
 
 router.use('/forget_password', forget_password);
 router.use('/login', login);
 router.use('/register', register);
 router.use('/reset_password', reset_password);
 router.use('/search', search);
-
+router.use('/upload', upload);
 router.use('/', root);
-var multer = require('multer');
 
-var Storage = multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, path.join(__dirname, "..", "..", "..", "..", "public", "uploads"));
-  },
-  filename: function (req, file, callback) {
-    callback(null, file.fieldname + "_" + uuid.v4() + "_" + file.originalname);
-  }
-});
 
-var upload = multer({
-  storage: Storage
-}
-).array("file", 5000); //Field name and max count
-
-router.post('/upload', async function (req, res) {
-
-  upload(req, res, function (err) {
-    if (err) {
-      console.log(err);
-      return res.end("Something went wrong!");
-    }
-    console.log("ok");
-    return res.end("File uploaded sucessfully!.");
-  });
-});
 
 module.exports = router;
---
+++ routes/api/v1/User/upload.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var mongoose = require('../../../../utils/mongo');
+var User = require('../../../../models/User');
+var config = require('../../../../utils/config');
+var jwt = require('jsonwebtoken');
+var router = express.Router();
+const fileUpload = require('express-fileupload');
+
+const uuid = require('uuid')
+const path = require('path');
+var multer = require('multer');
+
+var Storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, path.join(__dirname, "..", "..", "..", "..", "public", "uploads"));
+  },
+  filename: function (req, file, callback) {
+    console.log(1);
+    console.log(file);
+    // console.log(req);
+    callback(null, file.fieldname + "_" + uuid.v4() + "_" + file.originalname);
+  }
+});
+
+var upload = multer({
+  storage: Storage
+})
+  .array("file", 5000); //Field name and max count
+
+
+// router.post('/', upload.single('file'), (req, res) => {
+//   console.log("upload!");
+//   res.send();
+//   // upload(req, res, function (err) {
+//   //   if (err) {
+//   //     console.log(err);
+//   //     res.status(300);
+//   //     return res.end("Something went wrong!");
+//   //   }
+//   //   console.log("ok");
+//   //   res.status(200);
+//   //   return res.end("File uploaded sucessfully!.");
+//   // });
+// });
+
+
+router.post('/', async function (req, res) {
+  // console.log(req.body);
+  upload(req, res, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(300);
+      return res.end("Something went wrong!");
+    }
+    console.log("ok");
+    res.status(200);
+    return res.end("File uploaded sucessfully!.");
+  });
+});
+// router.use(fileUpload());
+// router.post('/', (req, res, next) => {
+//   console.log(req.body);
+//   // let imageFile = req.body.file;
+//   let imageFile = req.body.file;
+//   console.log("123");
+//   imageFile.mv(path.join(__dirname, "..", "..", "..", "..", "public", "uploads/")
+//     + uuid.v4() + `${req.body.filename}.jpg`, function (err) {
+//       if (err) {
+//         console.log(err);
+//         return res.status(500).send(err);
+//       }
+
+//       res.json({
+//         file: path.join(__dirname, "..", "..", "..", "..", "public", "uploads/")
+//           + `${req.body.filename}.jpg`
+//       });
+//     });
+
+// })
+
+module.exports = router;
