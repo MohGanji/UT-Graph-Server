9f6183d78dab48634eda728d6287f61ba9d465e1
hadi <m.hadi.hojjat@gmail.com>
2018-09-03 10:49:15 +0430

    Add api for read all of notifications

--- routes/api/v1/Notification/root.js
+++ routes/api/v1/Notification/root.js
@@ -12,9 +12,9 @@ router.get('/:status', isAuthenticated, async function (req, res) {
     var userId = user._id;
     var notifications;
     if (req.params.status == 1)
-      notifications = await Notification.find({ user: userId, read: false });
+      notifications = await Notification.find({ user: user.username, read: false });
     else
-      notifications = await Notification.find({ user: userId });
+      notifications = await Notification.find({ user: user.username });
   } catch (err) {
     return res.status(500).send();
   }
@@ -27,6 +27,22 @@ router.get('/:status', isAuthenticated, async function (req, res) {
 
   res.status(200).send(JSON.stringify({ data: mappedNotifications }));
 });
+
+router.post('/read-all', isAuthenticated, async function (req, res) {
+  let username = req.username;
+  console.log(username);
+  try {
+    await Notification.update(
+      { user: username },
+      { $set: { read: true } },
+      { "multi": true }
+    );
+  } catch (err) {
+    return res.status(500).send();
+  }
+
+  return res.status(200).send();
+})
 
 //   GET /:id/accept
 
