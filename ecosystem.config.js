f01bd35759608f2038ea889d8fe54047418eda8a
Mojtaba Shahbazi <mojtabashahbazi1377@gmail.com>
2018-09-11 17:38:12 +0430

    add / at end of PUBLIC_URL

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,28 @@
 module.exports = {
-  apps : [{
-    name      : 'UT-Graph-Server',
-    script    : 'app.js',
-    env: {
-      NODE_ENV: 'development',
-      PUBLIC_URL: 'http://localhost:8080/public'
+  apps: [
+    {
+      name: 'UT-Graph-Server',
+      script: 'app.js',
+      env: {
+        NODE_ENV: 'development',
+        PUBLIC_URL: 'http://localhost:8080/public/',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PUBLIC_URL: 'http://188.166.114.43/public/',
+      },
     },
-    env_production : {
-      NODE_ENV: 'production',
-      PUBLIC_URL:'http://188.166.114.43/public'
-    }
-  }],
+  ],
 
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+  deploy: {
+    production: {
+      user: 'node',
+      host: '212.83.163.1',
+      ref: 'origin/master',
+      repo: 'git@github.com:repo.git',
+      path: '/var/www/production',
+      'post-deploy':
+        'npm install && pm2 reload ecosystem.config.js --env production',
+    },
+  },
 };
