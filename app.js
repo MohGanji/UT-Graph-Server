b36fb83279259c50aea2cac24516ee652405f14f
hadi <m.hadi.hojjat@gmail.com>
2018-09-11 17:24:11 +0430

    Fix some bug

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const User = require('./models/User');
 var app = express();
 // router.use('/public', express.static(__dirname + '/public'));
 // router.use(express.static('public'))
-app.use(express.static(path.join(__dirname, "public", "uploads")))
+app.use('/public', express.static(path.join(__dirname, 'public', 'uploads')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,12 +25,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', api);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -38,7 +37,7 @@ app.use(function (req, res, next) {
 
 // error handler
 app.set('view engine', 'jade');
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
