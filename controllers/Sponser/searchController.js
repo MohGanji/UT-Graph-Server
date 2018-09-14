e779b2de44cb958f3e232d58fd50375b5672ad23
hadi <m.hadi.hojjat@gmail.com>
2018-09-14 14:50:04 +0430

    Add sponser controller

---
+++ controllers/Sponser/searchController.js
@@ -0,0 +1,33 @@
+var Sponser = require('../../models/Sponser');
+var normalizeImage = require('../../utils/normalizeImage');
+
+exports.sponserSearch = async function (req, res) {
+  let keyword = req.params.keyword;
+  let docs;
+
+  if (keyword.length < 2) {
+    return res.status(404).send();
+  }
+
+  try {
+    docs = await Sponser.find({
+      $or: [
+        { name: { $regex: '.*' + keyword + '.*' } },
+        { url: { $regex: '.*' + keyword + '.*' } }
+      ]
+    });
+  } catch (err) {
+    return res.status(500).send();
+  }
+
+  if (docs.length === 0) {
+    return res.status(404).send();
+  } else {
+    var mappedDocs = await Promise.all(
+      docs.map(async function (doc) {
+        return normalizeImage(doc.toJSON());
+      })
+    );
+    return res.status(200).send(JSON.stringify({ data: mappedDocs }));
+  }
+};
