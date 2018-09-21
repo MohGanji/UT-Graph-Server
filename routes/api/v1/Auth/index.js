1d994f08335d1f3a1ba832a1b8b88c1d68ef8731
Arman Rostami <arman.rostami.999@gmail.com>
2018-09-21 21:16:45 +0430

    Delete redundant test in  Auth route

--- routes/api/v1/Auth/index.js
+++ routes/api/v1/Auth/index.js
@@ -1,33 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var authController = require('../../../../controllers/auth/authController');
-var email = require('../../../../mail/');
-const nodemailer = require('nodemailer');
 
 router.post('/refreshToken', authController.refreshToken);
 router.post('/validateToken', authController.validateAccessToken);
-router.get('/test', async function (req, res) {
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: email.user,
-      pass: email.password
-    }
-  });
-  let mailOptions = {
-    from: email.user,
-    to: 'arman.magician@gmail.com',
-    subject: 'test4',
-    html: `<p align="right" dir="right">با سلام</p>
-    <p align="right" dir="right">برای فعالسازی حساب کاربری خود <a href="http://localhost:3000">اینجا</a> را کلیک کنید</p>`
-  };
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-      return res.status(500).send();
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
-});
+
 module.exports = router;
