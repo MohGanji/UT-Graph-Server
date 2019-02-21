93308fea9331ecaacee7334b0d864a37fa92d061
mhadih <m.hadi.hojjat@gmail.com>
2019-02-21 13:00:31 +0330

    Add build Dic

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const mongoose = require('./utils/mongo');
 var app = express();
 // router.use('/public', express.static(__dirname + '/public'));
 // router.use(express.static('public'))
-app.use('/public', express.static(path.join(__dirname, 'public', 'uploads')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
---
+++ build/precache-manifest.66acacde96fde073fee9da3461b7c3c0.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b86d8ba79c0b028ee2a1e41ae79af9a",
+    "url": "/static/media/logo.6b86d8ba.svg"
+  },
+  {
+    "revision": "306f5bcd0ff8b5f19a73",
+    "url": "/static/css/main.624032f9.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f69b23dd63f041fcd7ff091762677ea8",
+    "url": "/static/media/graph.f69b23dd.svg"
+  },
+  {
+    "revision": "f98864a4d42273210f8c",
+    "url": "/static/js/2.f98864a4.chunk.js"
+  },
+  {
+    "revision": "cb84dd7def151b035fcfdb2659d9989d",
+    "url": "/static/media/defaultProfile.cb84dd7d.svg"
+  },
+  {
+    "revision": "720c6989712ed293720825b2bbbd2c77",
+    "url": "/static/media/map1.720c6989.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "5fabd1725bc01005adb57571ddffc216",
+    "url": "/static/media/calender1.5fabd172.svg"
+  },
+  {
+    "revision": "12701c3158240da986dce362b791902a",
+    "url": "/static/media/group1.12701c31.svg"
+  },
+  {
+    "revision": "1ea4c07f0cf23a880eac5039a9b0c6aa",
+    "url": "/static/media/pencil1.1ea4c07f.svg"
+  },
+  {
+    "revision": "306f5bcd0ff8b5f19a73",
+    "url": "/static/js/main.306f5bcd.chunk.js"
+  },
+  {
+    "revision": "59c76c1857998f1f9c991da5b5bcbb8f",
+    "url": "/static/media/coverpic.59c76c18.svg"
+  },
+  {
+    "revision": "1279c6355a27a8bcac72c552acbb1d4f",
+    "url": "/static/media/resume.1279c635.svg"
+  },
+  {
+    "revision": "f77870ef961d7f5698c8cea820d0bb29",
+    "url": "/static/media/tag.f77870ef.svg"
+  },
+  {
+    "revision": "7bea6aeb99911ca49e2cf0dc20b5f892",
+    "url": "/static/media/hourglass1.7bea6aeb.svg"
+  },
+  {
+    "revision": "b9c7df4570eda9d7b1293cd975fbe5ba",
+    "url": "/static/media/hourglass2.b9c7df45.svg"
+  },
+  {
+    "revision": "c9e04390f7720519b0aaddaf12416fef",
+    "url": "/static/media/eventPageMap.c9e04390.png"
+  },
+  {
+    "revision": "741196589ac38b24118a1dbfea2a81ef",
+    "url": "/static/media/defaultProfile.74119658.jpg"
+  },
+  {
+    "revision": "dc295f6cc06da325742ce2470c491fac",
+    "url": "/static/media/cancel(1).dc295f6c.svg"
+  },
+  {
+    "revision": "3a22a5058e37bd6d0ddf7db0482db136",
+    "url": "/static/media/checked.3a22a505.svg"
+  },
+  {
+    "revision": "31cfb139632a81abead2e5584ce98374",
+    "url": "/static/media/utg.31cfb139.svg"
+  },
+  {
+    "revision": "e18ffdf4d1adec807d5a22525fc15f7b",
+    "url": "/static/media/email.e18ffdf4.svg"
+  },
+  {
+    "revision": "d6bebffb59b06520370f597615022b59",
+    "url": "/static/media/profilePic.d6bebffb.jpg"
+  },
+  {
+    "revision": "c4a1ad800b63450bee59a7f5853c6c0c",
+    "url": "/static/media/sky.c4a1ad80.jpg"
+  },
+  {
+    "revision": "f98864a4d42273210f8c",
+    "url": "/static/css/2.c5ae242d.chunk.css"
+  },
+  {
+    "revision": "0cca781624fef9ad092c57e525cdc062",
+    "url": "/index.html"
+  }
+];
